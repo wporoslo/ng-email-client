6151f0fe95c17eecc72d9a8e434fb06dcc503bc2
wporoslo <wporoslo@gmail.com>
2021-02-08 18:32:40 +0100

    initial scaffolding

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  { path: '', component: SigninComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
