098c4930e1e3a94623a1716e706a780b109228d7
wporoslo <wporoslo@gmail.com>
2021-02-09 12:37:43 +0100

    update generic type on auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface UsernameAvailableResponse {
+  available: boolean;
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -8,8 +11,11 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
-    return this.http.post<any>('https://api.angular-email.com/auth/username', {
-      username,
-    });
+    return this.http.post<UsernameAvailableResponse>(
+      'https://api.angular-email.com/auth/username',
+      {
+        username,
+      }
+    );
   }
 }
